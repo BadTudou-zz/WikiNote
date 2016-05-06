843c4849fd58956408e48d392adc1679463b79a8
jtaotao <1028155243@qq.com>
2016-05-06 14:57:18 +0800

    add manage.js
    1.添加：'manage.js'

---
+++ js/manage.js
@@ -0,0 +1,73 @@
+/*
+    Copyright © JiangTaoTao, 2016
+    All rights reserved
+
+    Name    :   manage.js
+    By      :   BadTudu
+    Date    :   2016年5月05日20:13:05
+    Note    :   WikiNote管理页面
+*/
+$(function() 
+{
+    //复杂菜单效果
+    $(".menu_list").hide();
+    $(".menu_list_first").show();
+    $(".a_list").click(function() 
+    {
+        var len = $('.a_list').length;
+        var index = $(".a_list").index(this);
+        for (var i = 0; i < len; i++) 
+        {
+            if (i == index) 
+            {
+                $('.menu_list').eq(i).slideToggle(300);
+            } 
+            else 
+            {
+                $('.menu_list').eq(i).slideUp(300);
+            }
+        }
+    });
+})
+
+//点击显示复杂菜单和隐藏简洁菜单
+$(function() 
+{
+    $(".menu-oc").click(function() 
+    {
+        $(".leftmenu1").animate(
+        {
+            left: "-292px"
+        });
+        $(".leftmenu2").animate(
+        {
+            left: "0px"
+        });
+        $(".rightcon").css("margin-left", "52px")
+    })
+    $(".menu-oc1").click(function() 
+    {
+        $(".leftmenu1").animate({
+            left: "0px"
+        });
+        $(".leftmenu2").animate({
+            left: "-192px"
+        });
+        $(".rightcon").css("margin-left", "240px");
+    })
+})
+
+//简单菜单移动效果
+$(function() 
+{
+    $(".j_menu_list").hide();
+    $(".j_a_list").hover(function() 
+    {
+        $(".leftmenu2 ul li").hover(function() 
+        {
+            $(this).find('.j_menu_list').show();
+        }, function() {
+            $(this).find('.j_menu_list').hide();
+        });
+    })
+})
